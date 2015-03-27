d1d36aeae743bd05396013eb7185f096bc7b167d
Alejandro <alejandrocerro@Alejandros-MacBook-Pro.local>
2015-03-27 17:00:35 -0500

    finished up to chapter 7

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  * MEAN stack from Learnable.com
  */
 
-var http = require('http');
+var http              = require('http'),
+    employeeService   = require('./lib/employees'),
+    responder         = require('./lib/responseGenerator'),
+    staticFile        = responder.staticFile('/public');
 
 http.createServer(function (req,res) {
   //A parsed url to work in case there are parameters
@@ -12,7 +15,40 @@ http.createServer(function (req,res) {
   //In case the client uses lower case for methods
   req.method = req.method.toUpperCase();
   console.log(req.method + ' ' + req.url);
-  res.end('The current time is ' + Date.now())
+
+  if(req.method !== 'GET'){
+    res.writeHead(501, {
+      "Content-Type": "text/plain"
+    });
+    return res.end(req.method + ' is not implemented by this server');
+  }
+
+  if(_url = /^\/employees$/i.exec(req.url)){
+    //return list of employees
+    employeeService.getEmployees(function (error,data) {
+      if(error){
+        return responder.send500(error, res);
+      }
+      return responder.sendJson(data,res);
+    })
+  }else if(_url = /^\/employees\/(\d+)$/i.exec(req.url)){
+    //find the employee by the id in the route
+    employeeService.getSingleEmployee(_url[1], function (error,data) {
+      if(error){
+        return responder.send500(error, res);
+      }
+      if(!data){
+        return responder.send404(res);
+      }
+      return responder.sendJson(data,res);
+    });
+  }else{
+    //try to send the static file, if it exists
+    //if not send 404
+    /**I added this, it wasn't in the book*/
+    responder.staticFile(staticFile);
+  }
+
 }).listen(1337, '127.0.0.1');
 
 console.log("Server running at http://127.0.0.1:1337");
